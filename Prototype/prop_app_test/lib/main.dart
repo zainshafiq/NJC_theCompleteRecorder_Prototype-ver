85a96e1136a6de1bb9c2e3470494e414255a7d34
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2021-02-02 22:00:03 -0600

    Commit

--- Prototype/prop_app_test/lib/main.dart
+++ Prototype/prop_app_test/lib/main.dart
@@ -570,7 +570,7 @@ class _RecordPageState extends State<Record>
               progress: _animationController,
               color: Colors.redAccent,
             ),
-            iconSize: 255,
+            iconSize: 260,
             splashColor: Colors.redAccent,
             onPressed: () => _handleOnPressed(),
           ),
