97835ce034532b3ce94f6dbc79f09cfff32d87e0
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2021-02-02 22:09:18 -0600

    Commit

--- Prototype/prop_app_test/lib/main.dart
+++ Prototype/prop_app_test/lib/main.dart
@@ -622,7 +622,7 @@ class Folder extends StatelessWidget {
                     //this widget houses the elements inside the card widget
                     leading: IconButton(
                       icon: Icon(Icons.home_rounded),
-                      iconSize: 70.0,
+                      iconSize: 75.0,
                       color: Colors.white,
                       highlightColor: Colors.grey,
                       onPressed: () {
