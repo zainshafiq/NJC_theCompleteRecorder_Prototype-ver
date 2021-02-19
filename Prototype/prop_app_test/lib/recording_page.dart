ae3e585e29aadc94c1e705cacef6b5c7941032f1
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2021-02-19 17:36:34 -0600

    Commit

---
+++ Prototype/prop_app_test/lib/recording_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RecordingPage extends StatefulWidget {
+  @override
+  _RecordingPageState createState() => _RecordingPageState();
+}
+
+class _RecordingPageState extends State<RecordingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
